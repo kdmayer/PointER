089d189aadcc9ef93116de35528bf725d69f132a
ADS\ga73pag <krapf@ftm.mw.tum.de>
2022-08-24 17:11:46 +0200

    file path adaptions

--- experimentation/Point_Cloud_Demo.py
+++ experimentation/Point_Cloud_Demo.py
@@ -1,48 +1,50 @@
-
 import geopandas as gpd
 import numpy as np
 
 import os
 import shapely
 import laspy
-import sys
 
-import utils as utils
+from utils import utils as utils
 import utils.visualization as visualization
 from experimentation.pdal_pipeline import run_pdal_pipeline
 
 
 # Define project base directory
-DIR_BASE = os.path.dirname(sys.argv[0])
-assert DIR_BASE.split("/")[-1] == 'assets', "You are not in the assets directory"
+DIR_BASE = os.getcwd()
+DIR_ASSETS = os.path.join(os.path.dirname(DIR_BASE), 'assets')
+assert DIR_ASSETS.split("\\")[-1] == 'assets', "You are not in the assets directory"
 
 # Configuration
-LAS_FILE_NAME = "SP3278_P_11321_20171123_20171123.las"
-POINT_COUNT_THRESHOLD = 2000
+LAS_FILE_PATH = os.path.join(DIR_ASSETS, "SP3025_P_10700_20190328_20190328.las")
+POINT_COUNT_THRESHOLD = 1000
 
 # Run subsample test
-test = utils.subsample_las(LAS_FILE_NAME)
-print(test)
+# test = utils.subsample_las(LAS_FILE_PATH)
+# print(test)
 
 # Import footprint polygons
-osm_footprints = gpd.read_file("coventry_building_footprints.geojson")
+FOOTPRINTS_FILE_PATH = os.path.join(DIR_ASSETS, "chipping_norton_building_footprints.geojson")
+osm_footprints = gpd.read_file(FOOTPRINTS_FILE_PATH)
+
+# Reproject OSM footprints to EPSG:27700, if necessary
+osm_footprints = osm_footprints.to_crs("EPSG:27700")
+#osm_footprints.to_file("coventry_building_footprints.geojson", driver='GeoJSON')
 
 # Buffer polygons a bit to capture the building footprint better
 osm_footprints["geometry"] = osm_footprints["geometry"].buffer(1)
 
-# Reproject OSM footprints to EPSG:27700, if necessary
-#osm_footprints = osm_footprints.to_crs("EPSG:27700")
-#osm_footprints.to_file("coventry_building_footprints.geojson", driver='GeoJSON')
-
 footprint_list = osm_footprints['geometry'].tolist()
 
 # Select only polygons which are within the LiDAR tile and save their WKT string
-lidar_bounding_box = utils.create_tile_bounding_box(LAS_FILE_NAME)
+lidar_bounding_box = utils.create_tile_bounding_box(LAS_FILE_PATH)
 polys = [elem.wkt for elem in footprint_list if isinstance(elem, shapely.geometry.polygon.Polygon) and lidar_bounding_box.contains(elem.centroid)]
 print(f"Number of relevant polygons: {len(polys)}")
 
 # crop LiDAR data in footprints using pdal
-run_pdal_pipeline(footprint_list=polys, las_file_path=LAS_FILE_NAME, random_sample_size=15)
+sample_size = 10
+run_pdal_pipeline(footprint_list=polys, las_file_path=LAS_FILE_PATH, random_sample_size=sample_size)
+print("Process ran: %s seconds \n That is ~ %s per building footprint", (stop_time, int(stop_time/sample_size)))
 
 # load and postprocess cropped LiDAR point cloud examples
 point_cloud_examples = []
