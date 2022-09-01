3b27bd219b222476f463725b0656b9143f9e49df
ADS\ga73pag <krapf@ftm.mw.tum.de>
2022-09-01 14:51:46 +0200

    restructured normalization code

--- utils/utils.py
+++ utils/utils.py
@@ -18,13 +18,6 @@ def _convert_numpy_to_las(x: np.ndarray = None, header=None):
     return outfile
 
 
-def _sample_random_points(x: np.ndarray = None, random_sample_size: int = None):
-    rng = np.random.default_rng()
-    lidar_subset = rng.choice(a=x, size=random_sample_size, replace=False, axis=0)
-
-    return lidar_subset
-
-
 def _convert_las_to_numpy(las_data=None):
     lidar_numpy = np.array((las_data.x,
                             las_data.y,
@@ -32,8 +25,24 @@ def _convert_las_to_numpy(las_data=None):
                             las_data.intensity,
                             las_data.raw_classification,
                             las_data.scan_angle_rank)).transpose()
-
     return lidar_numpy
+
+
+def _convert_multipoint_to_numpy(mp: shapely.geometry.MultiPoint = None):
+    lidar_numpy = np.array([(g.x, g.y, g.z) for g in mp.geoms])
+    return lidar_numpy
+
+
+def _convert_numpy_to_multipoint(lidar_numpy: np.ndarray = None):
+    assert lidar_numpy.shape[0] == 3, 'unexpected shape of array. expected shape is (3, :)'
+    mp = shapely.geometry.MultiPoint(lidar_numpy)
+    return mp
+
+
+def _sample_random_points(x: np.ndarray = None, random_sample_size: int = None):
+    rng = np.random.default_rng()
+    lidar_subset = rng.choice(a=x, size=random_sample_size, replace=False, axis=0)
+    return lidar_subset
 
 
 def subsample_las(original_las_data_filepath: str = None, random_sample_size: int = 100000):
@@ -57,48 +66,29 @@ def subsample_las(original_las_data_filepath: str = None, random_sample_size: in
     return outfile
 
 
-def normalize_geom(geom: shapely.geometry = None, scaling_factor: int = 1000, random_sample_size: int = None):
-    if len(geom.geoms) >= random_sample_size:
-        # scale x coordinates
-        new_x = np.array([g.x for g in geom.geoms])
-        new_x = (new_x - new_x.min()) / scaling_factor
-        # scale y coordinates
-        new_y = np.array([g.y for g in geom.geoms])
-        new_y = (new_y - new_y.min()) / scaling_factor
-        # scale z coordinates
-        new_z = np.array([g.z for g in geom.geoms])
-        new_z = (new_z - new_z.min()) / scaling_factor
-        # create numpy array from coordinates
-        lidar_numpy = np.array((new_x, new_y, new_z)).transpose()
-        # subsample numpy array
-        lidar_numpy = _sample_random_points(lidar_numpy, random_sample_size)
-    else:
-        lidar_numpy = []
-        print('not enough points')
-    return lidar_numpy
-
-
-def normalize_point_cloud_gdf(gdf_pc, scaling_factor: int = 1000, random_sample_size: int = None):
-    # lidar_numpy_list = []
-    # normalize to values between 0 and 1
-    # for i, geom in enumerate(gdf_pc.geom):
-    #     print(i)
-    #     if len(geom.geoms) >= random_sample_size:
-    #         lidar_numpy = normalize_geom(geom, scaling_factor, random_sample_size)
-    #         lidar_numpy_list.append(lidar_numpy)
-    #     else:
-    #         print('sample ' + str(i) + ' skipped because number of points is lower than required sample size')
-
-    lidar_numpy_list = list(gdf_pc.geom.apply(normalize_geom, args=[scaling_factor, random_sample_size]))
-
-    return lidar_numpy_list
-
-
-
 def create_tile_bounding_box(original_las_data_filepath: str = None):
     las_data = laspy.read(original_las_data_filepath)
     min_x, min_y, min_z, max_x, max_y, max_z = [*las_data.header.min, *las_data.header.max]
     return box(minx=min_x, miny=min_y, maxx=max_x, maxy=max_y)
+
+
+def normalize_geom(geom: shapely.geometry = None, scaling_factor: int = 1000, random_sample_size: int = None):
+    # convert multipoint to numpy array
+    lidar_numpy = _convert_multipoint_to_numpy(geom)
+    # scale x, y, z coordinates (0, 1, 2) according to scaling factor
+    for i in np.arange(0, 3):
+        lidar_numpy[:, i] = (lidar_numpy[:, i] - lidar_numpy[:, i].min()) / scaling_factor
+    # subsample numpy array
+    lidar_numpy = _sample_random_points(lidar_numpy, random_sample_size)
+
+    return lidar_numpy
+
+
+def normalize_point_cloud_gdf(gdf_pc, scaling_factor: int = 1000, random_sample_size: int = None):
+    # apply normalization function to entire dataframe
+    lidar_numpy_list = list(gdf_pc.geom.apply(normalize_geom, args=[scaling_factor, random_sample_size]))
+    return lidar_numpy_list
+
 
 
 
