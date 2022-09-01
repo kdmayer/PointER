599455293c1fb58bf4f58ad74396678beb13e381
ADS\ga73pag <krapf@ftm.mw.tum.de>
2022-09-01 07:34:32 +0200

    added normalization function

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ from shapely.geometry import box
 import numpy as np
 
 import laspy
-
+import shapely
 
 
 def _convert_numpy_to_las(x: np.ndarray = None, header=None):
@@ -57,7 +57,49 @@ def subsample_las(original_las_data_filepath: str = None, random_sample_size: in
     return outfile
 
 
+def normalize_geom(geom: shapely.geometry = None, scaling_factor: int = 1000, random_sample_size: int = None):
+    if len(geom.geoms) >= random_sample_size:
+        # scale x coordinates
+        new_x = np.array([g.x for g in geom.geoms])
+        new_x = (new_x - new_x.min()) / scaling_factor
+        # scale y coordinates
+        new_y = np.array([g.y for g in geom.geoms])
+        new_y = (new_y - new_y.min()) / scaling_factor
+        # scale z coordinates
+        new_z = np.array([g.z for g in geom.geoms])
+        new_z = (new_z - new_z.min()) / scaling_factor
+        # create numpy array from coordinates
+        lidar_numpy = np.array((new_x, new_y, new_z)).transpose()
+        # subsample numpy array
+        lidar_numpy = _sample_random_points(lidar_numpy, random_sample_size)
+    else:
+        lidar_numpy = []
+        print('not enough points')
+    return lidar_numpy
+
+
+def normalize_point_cloud_gdf(gdf_pc, scaling_factor: int = 1000, random_sample_size: int = None):
+    # lidar_numpy_list = []
+    # normalize to values between 0 and 1
+    # for i, geom in enumerate(gdf_pc.geom):
+    #     print(i)
+    #     if len(geom.geoms) >= random_sample_size:
+    #         lidar_numpy = normalize_geom(geom, scaling_factor, random_sample_size)
+    #         lidar_numpy_list.append(lidar_numpy)
+    #     else:
+    #         print('sample ' + str(i) + ' skipped because number of points is lower than required sample size')
+
+    lidar_numpy_list = list(gdf_pc.geom.apply(normalize_geom, args=[scaling_factor, random_sample_size]))
+
+    return lidar_numpy_list
+
+
+
 def create_tile_bounding_box(original_las_data_filepath: str = None):
     las_data = laspy.read(original_las_data_filepath)
     min_x, min_y, min_z, max_x, max_y, max_z = [*las_data.header.min, *las_data.header.max]
     return box(minx=min_x, miny=min_y, maxx=max_x, maxy=max_y)
+
+
+
+
