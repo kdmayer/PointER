2d41fb488f7c912a9b27c5b2141b1002141ec510
ADS\ga73pag <krapf@ftm.mw.tum.de>
2022-09-01 14:52:01 +0200

    added visualization for one building point cloud

--- utils/visualization.py
+++ utils/visualization.py
@@ -3,6 +3,7 @@ from typing import List
 import plotly.graph_objects as go
 import numpy as np
 
+import os
 
 def visualize_3d_array(point_cloud_array: np.ndarray = None, file_name_list: List = None, example_ID=None):
     x = point_cloud_array[example_ID, :, 0].flatten()
@@ -14,4 +15,24 @@ def visualize_3d_array(point_cloud_array: np.ndarray = None, file_name_list: Lis
     layout = go.Layout(title=f'Visualization of {file_name_list[example_ID]}')
     fig = go.Figure(data=[scatter], layout=layout)
     fig.show()
+    return
+
+
+def visualize_single_3d_point_cloud(point_cloud_array: np.ndarray = None, title: str = 'title', save_dir: str = None,
+                                    show: bool = False):
+    x = point_cloud_array[:, 0].flatten()
+    y = point_cloud_array[:, 1].flatten()
+    z = point_cloud_array[:, 2].flatten()
+
+    scatter = go.Scatter3d(x=x, y=y, z=z, mode='markers',
+                           marker=dict(size=3, color=z, colorscale='Viridis'))
+    layout = go.Layout(title=f'Visualization of {title}')
+    fig = go.Figure(data=[scatter], layout=layout)
+
+    if show:
+        fig.show()
+
+    if save_dir != None:
+        save_path = os.path.join(save_dir, title + '.html')
+        fig.write_html(save_path)
     return
