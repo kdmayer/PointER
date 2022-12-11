b1a31763ef7520cff039b9c4b9a9a662e566fc71
ADS\ga73pag <sebastian.krapf@tum.de>
2022-12-10 19:02:08 -0800

    added batch visualization

--- utils/visualization.py
+++ utils/visualization.py
@@ -52,3 +52,24 @@ def visualize_example_pointclouds(lidar_numpy_list: list, DIR_VISUALIZATION: str
                 save_dir=DIR_VISUALIZATION,
                 show=False
             )
+
+
+def batch_visualization(DIR_POINT_CLOUDS, DIR_SAVE, format: str='html', number_examples=None):
+    pc_file_names = os.listdir(DIR_POINT_CLOUDS)
+    if number_examples == None:
+        number_examples = len(pc_file_names)
+    count = 0
+    for pc_file_name in pc_file_names:
+        if count <= number_examples:
+            count += 1
+            pc_file_path = os.path.join(DIR_POINT_CLOUDS, pc_file_name)
+            point_cloud_array = np.load(pc_file_path)
+            # normalize pc array for visualization
+            for i in np.arange(0, 3):
+                point_cloud_array[:, i] = (point_cloud_array[:, i] - point_cloud_array[:, i].min())
+
+            save_path = os.path.join(DIR_SAVE, str(pc_file_name[:-4] + '.' + format))
+            visualize_single_3d_point_cloud(point_cloud_array, title=pc_file_name, save_path=save_path,
+                                            show=False, format=format)
+    return
+
